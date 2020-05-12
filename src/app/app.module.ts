3cf825757d64c01e9435ffb107bba3b10955cb97
maxi <maxiranqui@gmail.com>
2020-05-12 15:01:05 -0300

    Se genera formulario salud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SaludComponent } from './formulario/salud/salud.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SaludComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/formulario/salud/salud.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+
+import { FormControl, FormGroup, FormBuilder, Validator, Validators, FormArray } from '@angular/forms';
+
+@Component({
+  selector: 'app-salud',
+  templateUrl: './salud.component.html',
+  styleUrls: ['./salud.component.css']
+})
+export class SaludComponent implements OnInit {
+
+  constructor(private fb: FormBuilder) { }
+
+  hospitalForm = this.fb.group({
+		nombrehosp: [" ",Validators.required ],
+		direccion: this.fb.group({
+			callehosp: [ '' ],
+			numerohosp: [ '' ]
+		}),
+	});
+
+  pacienteForm = this.fb.group({
+		nombre: [ '', Validators.required ],
+		apellido: [ '' ],
+		edad: [ '' ],
+		direccion: this.fb.group({
+			calle: [ '' ],
+			numero: [ '' ]
+		}),
+		telefonos: this.fb.array([ this.fb.control('') ])
+	});
+
+	get telefonos() {
+		return this.pacienteForm.get('telefonos') as FormArray;
+	}
+
+	agregarTelefono() {
+		this.telefonos.push(this.fb.control(''));
+	}
+
+	submit() {
+		this.pacienteForm.value();
+
+		this.pacienteForm.setValue({
+			nombre: 'Diego',
+			apellido: 'Maradona',
+			edad: 59,
+			direccion: {
+				calle: 'Perez'
+			},
+			telefonos: [ '' ]
+		});
+
+		this.pacienteForm.patchValue({ edad: 60 });
+	}
+
+  
+  ngOnInit(): void {
+  }
+
+}
